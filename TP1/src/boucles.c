20cdfe6fb259652c5ecd3a95f835ed362f1b69fe
Diime95 <mezouar.mehdi95800@gmail.com>
2026-09-18 09:32:07 +0000

    push 1.6 bis

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,24 +1,30 @@
 int main() {
 
     int compteur = 5;
+    int i = 1;
 
     if (compteur >= 10) {
         printf("Le compteur doit etre inferieur a 10.\n");
         return 1;
     }
 
-    for (int i = 1; i <= compteur; i++) {
+    while (i <= compteur) {
 
-        for (int j = 1; j <= i; j++) {
+        int j = 1;
+
+        while (j <= i) {
 
             if (j == 1 || j == i || i == compteur) {
                 printf("* ");
             } else {
                 printf("# ");
             }
+
+            j++;
         }
 
         printf("\n");
+        i++;
     }
 
     return 0;
